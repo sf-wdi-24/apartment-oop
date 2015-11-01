e6cb4bc173befaaa936979117c52ae8c6f1a5b47
cameronjacoby <cameron.jacoby@gmail.com>
2015-10-31 20:01:48 -0700

    add solution for manager

--- src/people/manager.js
+++ src/people/manager.js
@@ -4,14 +4,32 @@ var Person = require('./person'),
 function Manager(name, contact) {
   this.properties = [];
   // set attributes (name, contact) using `this`
+  this.name = name;
+  this.contact = contact;
 }
+
+Manager.prototype = new Person();
+Manager.prototype.constructor = Manager;
 
 Manager.prototype.addProperty = function(property) {
   // add property to manager's properties
+  if (property instanceof Property) {
+    this.properties.push(property);
+  } else {
+    console.error("Failed to add property:", property);
+  }
+  return this;
 };
 
 Manager.prototype.removeProperty = function(property) {
   // remove property from manager's properties
+  var indexOfProperty = this.properties.indexOf(property);
+  if(indexOfProperty > -1) {
+    this.properties.splice(indexOfProperty, 1);
+  } else {
+    console.error("Failed to remove property:", property);
+  }
+  return this;
 };
 
 module.exports = Manager;
