1c982423bbcc4198a0243942ba5b6c8bbc39ee8f
alanlee35 <alanblee12@gmail.com>
2015-11-05 15:17:23 -0800

    set up inheritance between person, manager and tenant

--- src/people/tenant.js
+++ src/people/tenant.js
@@ -3,9 +3,13 @@ var Person = require('./person');
 function Tenant(name, contact) {
   this.references = [];
   // set attributes (name, contact) using `this`
+  Person.call(this, name, contact);
 }
 
 // Tenant inherits from Person
+Tenant.prototype = new Person;
+Tenant.prototype.constructor = Tenant;
+
 
 Tenant.prototype.addReference = function(reference) {
   // add reference (which is instance of `Person`) to tenant's references
