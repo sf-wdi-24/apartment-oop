2862849fc816f92a28db920c44fdf955f4f7f409
Jsdwyer26 <Jsdwyer26@gmail.com>
2015-11-05 21:48:44 -0800

    apartment-oop homework

--- src/people/manager.js
+++ src/people/manager.js
@@ -10,11 +10,22 @@ var Person = require('./person'),
 
 
 // Manager inherits from Person
+//Step 1 of adding inheritance
+Manager.prototype = new Person();
+
+//Step 2 for setting prototype inhertiance
+//Set constructor of subclass to it's constructor function
+Manager.prototype.constructor = Manager;
 
 Manager.prototype.addApartment = function(apartment) {
-  // add apartment to manager's apartments
+	// add apartment to manager's apartments
+	return this.apartments.push(apartment);
 };
 
 Manager.prototype.removeApartment = function(apartment) {
   // remove apartment from manager's apartments
+  var index = this.apartments.indexOf(apartment);
+  //removing var index from apartments
+  this.apartments.splice(index, 1);
 };
+
