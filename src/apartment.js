5fd58588940c896d1296ba36a961681b48a8966a
TheEddyChang <ehchang58@yahoo.com>
2015-11-05 21:59:34 -0800

    apartment hw

--- src/apartment.js
+++ src/apartment.js
@@ -7,32 +7,65 @@ function Apartment(address) {
 module.exports = Apartment;
 
 var Manager = require('./people/manager'),
-    Tenant = require('./people/tenant'),
-    Unit = require('./unit');
+  Tenant = require('./people/tenant'),
+  Unit = require('./unit');
 
 
 Apartment.prototype.setManager = function(manager) {
   // if `manager` is an instance of `Manager`
   // set `this.manager` to `manager`
-};
+  if (manager instanceof Manager) {
+   this.manager = manager;
+  }
+  };
+
 
 Apartment.prototype.getManager = function() {
   // return `this.manager`
+  return this.manager;
 };
 
 Apartment.prototype.addTenant = function(unit, tenant) {
   // if apartment has a manager and tenant has 2 references
   // add tenant to unit
+  if (this.manager !== null && tenant.references.length > 1) {
+     unit.tenant = tenant;
+     }
+  
 };
 
 Apartment.prototype.removeTenant = function(unit, tenant) {
   // remove tenant from unit
+  unit.tenant = null;
+  
 };
 
 Apartment.prototype.availableUnits = function() {
   // return num of available units
+   var openUnit = 0;
+   this.units.forEach(function(available) {
+    if (available.tenant === null) {
+      openUnit += 1;
+    }
+   });
+     return openUnit;
 };
 
 Apartment.prototype.rentedUnits = function() {
   // return num of rented units
-};
+  var rent = 0;
+   this.units.forEach(function(rents) {
+    if (rents.tenant !== null) {
+      rent += 1;
+    }
+     
+   });
+   return rent;
+    
+    };
+  
+
+
+
+
+    
--- src/people/manager.js
+++ src/people/manager.js
@@ -1,7 +1,13 @@
 function Manager(name, contact) {
   this.apartments = [];
   // set attributes (name, contact) using `this`
+  this.name = name;
+  this.contact = contact;
 }
+ 
+
+
+
 
 module.exports = Manager;
 
@@ -10,11 +16,19 @@ var Person = require('./person'),
 
 
 // Manager inherits from Person
+Manager.prototype = new Person();
+Manager.prototype.constructor = Manager;
 
 Manager.prototype.addApartment = function(apartment) {
   // add apartment to manager's apartments
+
+  return this.apartments.push(apartment);
 };
 
 Manager.prototype.removeApartment = function(apartment) {
   // remove apartment from manager's apartments
+  var index = this.apartments.indexOf(apartment);
+
+  this.apartments.splice(index, 1);
+
 };
