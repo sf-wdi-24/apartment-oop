6f90adf6ec4826fb050513a1458e916648caa951
stevennoble78 <stevennoble78@hotmail.com>
2015-11-05 17:13:46 -0800

    check out my NEW apartment app

--- src/people/person.js
+++ src/people/person.js
@@ -1,5 +1,6 @@
 function Person(name, contact) {
-  // set attributes (name, contact) using `this`
+  this.name = name;
+  this.contact = contact;
 }
 
 module.exports = Person;
--- src/people/tenant.js
+++ src/people/tenant.js
@@ -1,14 +1,17 @@
 var Person = require('./person');
 
 function Tenant(name, contact) {
+	Person.call(this, name, contact);
   this.references = [];
-  // set attributes (name, contact) using `this`
 }
 
 // Tenant inherits from Person
+Tenant.prototype = new Person();
+Tenant.prototype.constructor = Tenant;
 
 Tenant.prototype.addReference = function(reference) {
   // add reference (which is instance of `Person`) to tenant's references
+  this.references.push(reference);
 };
 
 module.exports = Tenant;
